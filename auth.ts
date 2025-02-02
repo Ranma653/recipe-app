cd1bb511a67f24d9c11215b717dc3f8c960e43aa
Alexis Black <Alexisblack2b2icloud.com>
2025-02-01 20:48:44 -0600

    decided to remove next-auth

--- auth.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github"
-
-
-//exporting the handlers and sign in passing in all our proviers
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers:[GitHub],
-})
